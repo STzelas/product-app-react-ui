e3e4f088c369e3a70e3ef8f8dd6c5eb4c5d8ae58
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-25 20:59:26 +0300

    feat: register success added

---
+++ src/pages/RegistrationSuccess.tsx
@@ -0,0 +1,11 @@
+const RegistrationSuccess = () => {
+  return (
+    <>
+      <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Registration Successful
+      </h3>
+    </>
+  )
+}
+
+export default RegistrationSuccess;

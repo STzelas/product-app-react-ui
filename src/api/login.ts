286864c4ee958d595c51cb8438be22bc8b4b05ef
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:01:32 +0300

    feat: loginSchema and types moved to type file

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,16 +1,4 @@
-import { z } from "zod"
-
-export const loginSchema = z.object({
-  username: z.string().min(1, {error: "Username is required"}),
-  password: z.string().min(1, {error: "Password is required"}),
-})
-
-export type LoginFields = z.infer<typeof loginSchema>
-
-export type LoginResponse = {
-  access_token: string,
-  token_type: string,
-}
+import { type LoginFields, type LoginResponse} from "@/types/types.tsx";
 
 export async function login ({username, password}: LoginFields):Promise<LoginResponse> {
   // const form = new URLSearchParams();

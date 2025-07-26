abefa5d0d4b4698b77d8e274e99498b13b1dd4bf
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 14:39:30 +0300

    feat: login api with zod added

---
+++ src/api/login.ts
@@ -0,0 +1,38 @@
+import { z } from "zod"
+
+export const loginSchema = z.object({
+  username: z.string().min(1, {error: "Username is required"}),
+  password: z.string().min(1, {error: "Password is required"}),
+})
+
+export type LoginFields = z.infer<typeof loginSchema>
+
+export type LoginResponse = {
+  access_token: string,
+  token_type: string,
+}
+
+export async function login ({username, password}: LoginFields):Promise<LoginResponse> {
+  // const form = new URLSearchParams();
+  // form.append("username", username);
+  // form.append("password", password);
+  //
+  // const res = await fetch(import.meta.env.VITE_API_URL + "/login/access-token",
+  //   {
+  //   method: "POST",
+  //   headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }),
+  //   body: form.toString()
+  //   }
+  // );
+  // if (!res.ok) {
+  //   let detail = "Login failed.";
+  //   try {
+  //     const data = await res.json()
+  //     if (typeof data?.detail === "string") detail = data.detail
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  //   throw new Error(detail);
+  // }
+  // return await res.json();
+}

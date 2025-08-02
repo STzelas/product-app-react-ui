aa16a91812a2579f0c40720402da28fc7aef539f
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-02 23:47:08 +0300

    chore: commented out removed

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,26 +1,26 @@
 import { type LoginFields, type LoginResponse} from "@/types/types.tsx";
 
 export async function login ({username, password}: LoginFields):Promise<LoginResponse> {
-  // const form = new URLSearchParams();
-  // form.append("username", username);
-  // form.append("password", password);
-  //
-  // const res = await fetch(import.meta.env.VITE_API_URL + "/login/access-token",
-  //   {
-  //   method: "POST",
-  //   headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }),
-  //   body: form.toString()
-  //   }
-  // );
-  // if (!res.ok) {
-  //   let detail = "Login failed.";
-  //   try {
-  //     const data = await res.json()
-  //     if (typeof data?.detail === "string") detail = data.detail
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  //   throw new Error(detail);
-  // }
-  // return await res.json();
+  const form = new URLSearchParams();
+  form.append("username", username);
+  form.append("password", password);
+
+  const res = await fetch(import.meta.env.VITE_API_URL + "/login/access-token",
+    {
+    method: "POST",
+    headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }),
+    body: form.toString()
+    }
+  );
+  if (!res.ok) {
+    let detail = "Login failed.";
+    try {
+      const data = await res.json()
+      if (typeof data?.detail === "string") detail = data.detail
+    } catch (err) {
+      console.error(err);
+    }
+    throw new Error(detail);
+  }
+  return await res.json();
 }

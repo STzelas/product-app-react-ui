327d86b54a48ddbb7e3040d7cf094ee29402923b
Sot. Tzelatidis <stzelati@gmail.com>
2025-08-02 23:46:28 +0300

    refactor: ctx -> authContext

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext} from "@/context/AuthContext.ts";
 
 export function useAuth() {
-  const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error("useAuth must be used within AuthProvider");
-  return ctx;
+  const authContext = useContext(AuthContext);
+  if (!authContext) throw new Error("useAuth must be used within AuthProvider");
+  return authContext;
 }
